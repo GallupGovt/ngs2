57d4de3816cefaa8a844d66e8c5699ff9dc9c05e
Pablo Diego-Rosell <pablo_diego-rosell@gallup.com>
2017-08-15 12:59:34 +0200

    One Master script to rule them all.
    We will use this master R script for the whole Cycle 1 analytics pipeline. The Master will: 
    1. Set the working directory ("setwd") for the entire workflow. 
    2. Run all package installs.
    3. Source all sub-scripts.

---
+++ NGS2_WITNESS_Cycle1_MASTER.R
@@ -0,0 +1,59 @@
+#Created by Pablo Diego Rosell, PhD, for Gallup inc. on August 2017
+
+### System information
+
+a=Sys.info()
+print(a)
+b=sessionInfo(package = NULL)
+print(a)
+
+### Set base directory
+
+setwd(X:/DARPA_NGS2/CONSULTING/Analytics/Cycle1)
+rm(list=ls())
+
+### Load all required packages
+
+if (!require("pacman")) install.packages("pacman")
+library ("pacman")
+pacman::p_load(multiwayvcov, lmtest, dplyr, reshape2, multiwayvcov, lmtest, Hmisc, corrgram, PerformanceAnalytics, 
+               doBy, car, ggplot2, DT, utils, lme4, rstan, rstanarm)
+
+### Process empanelment data
+
+source ("NGS2_WITNESS_empanelment_prep.R")
+
+### Process breadboard data
+
+source ("NGS2_WITNESS_Cycle1_experiment_prep.R")
+
+### Process coloring game data
+
+source ("NGS2_WITNESS_Cycle1_coloring_prep.R") ### TO BE ADDED (Pablo)
+
+### Response funnel 
+
+source ("NGS2_WITNESS_Cycle1_response.R") ### TO BE ADDED (Matt)
+
+
+### EXPERIMENT 1
+### Analyze experiment 1
+
+source ("NGS2_WITNESS_Cycle1_exploratory_exp1.R")
+source ("NGS2_WITNESS_Cycle1_machine_exp1.R")  ### TO BE ADDED (Anu)
+source ("NGS2_WITNESS_Cycle1_confirmatory_exp1.R")
+source ("NGS2_WITNESS_Cycle1_bayesian_exp1.R")
+
+### Analyze coloring game
+
+source ("NGS2_WITNESS_Cycle1_coloring.R")  ### TO BE ADDED (Pablo)
+
+
+### EXPERIMENT 2
+### Analyze experiment 2 
+
+source ("NGS2_WITNESS_Cycle1_exploratory_exp1.R")
+source ("NGS2_WITNESS_Cycle1_machine_exp2.R")  ### TO BE ADDED (Anu)
+source ("NGS2_WITNESS_Cycle1_confirmatory_exp1.R")
+source ("NGS2_WITNESS_Cycle1_bayesian_exp2.R")  ### TO BE ADDED (Pablo)
+

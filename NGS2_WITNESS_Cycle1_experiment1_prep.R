fbcb3a75d8bed98ac9e71c7bf6d28e286ca96e61
Matt Hoover <matthew.a.hoover@gmail.com>
2017-09-11 16:04:03 -0400

    Finish refactor and changes for experiment 1.
    This commit addresses the concerns around how participants are
    tied to one another. Before, ties were getting double-counted
    in many cases, due to how data was represented in Breadboard. The
    commit deals with this and also addresses destroying ties when
    someone leaves an experiment. Additionally, it drops experiments
    that don't meet certain criteria in terms of number of players or
    rounds.

--- NGS2_WITNESS_Cycle1_experiment1_prep.R
+++ NGS2_WITNESS_Cycle1_experiment1_prep.R
@@ -170,14 +170,16 @@ build_rewire <- function(changes) {
     # start building the `rewire` output file
     res <- lapply(changes, function(x) {
         if(nrow(x) > 0) {
-            tmp <- x[
+            tmp <- subset(x, redundant == 0)
+            tmp <- tmp[
                 order(x$round, x$pid, x$nid),
-                c('round', 'pid', 'nid', 'action_num')
+                c('round', 'pid', 'nid', 'overall_action')
             ]
 
-            tmp$break_tie <- ifelse(tmp$action_num == -1, 1, 0)
-            tmp$make_tie <- ifelse(tmp$action_num == 1, 1, 0)
-            tmp$action_num <- ifelse(tmp$action != 0, 1, tmp$action_num)
+            tmp$break_tie <- ifelse(tmp$overall_action == -1, 1, 0)
+            tmp$make_tie <- ifelse(tmp$overall_action == 1, 1, 0)
+            tmp$overall_action <- ifelse(tmp$overall_action != 0, 1, 0)
+            names(tmp)[grep('action$', names(tmp))] <- 'action_num'
 
             return(tmp)
         } else {
@@ -187,31 +189,243 @@ build_rewire <- function(changes) {
     return(res)
 }
 
-calculate_connections <- function(rounds) {
-    # count up number of connections by id by round
-    return(lapply(rounds, function(x) {
-        if(length(x) > 0) {
-            tmp <- do.call(rbind, mapply(function(y, rd) {
-                tmp1 <- aggregate(y$playerId2, by = list(y$playerId1), length)
-                tmp2 <- aggregate(y$playerId1, by = list(y$playerId2), length)
-                tmp <- merge(tmp1, tmp2, by = 'Group.1', all = TRUE)
-                tmp$num_neighbors <- apply(tmp[, grep('^x', names(tmp))], 1, function(z) {
-                    sum(z, na.rm = TRUE)
-                })
-                tmp$round <- rd
-                tmp <- tmp[, c('round', 'Group.1', 'num_neighbors')]
-                names(tmp)[2] <- 'pid'
-                return(tmp)
-            }, x, 1:length(x), SIMPLIFY = FALSE))
-            return(tmp)
+build_round_change_frame <- function(d) {
+    nid <- aggregate(d$overall_action, by = list(d$round, d$nid), sum)
+    pid <- aggregate(d$overall_action, by = list(d$round, d$pid), sum)
+    res <- merge(nid, pid, by = c('Group.1', 'Group.2'), all = TRUE)
+    res[is.na(res)] <- 0
+    res$conns <- res$x.x + res$x.y
+    return(res[, c('Group.1', 'Group.2', 'conns')])
+}
+
+build_start_frame <- function(d) {
+    p1 <- aggregate(d$playerId1, by = list(d$playerId1), length)
+    p2 <- aggregate(d$playerId2, by = list(d$playerId2), length)
+    res <- merge(p1, p2, by = 'Group.1', all = TRUE)
+    res[is.na(res)] <- 0
+    res$conns <- res$x.x + res$x.y
+    return(res[, c('Group.1', 'conns')])
+}
+
+changes_by_round <- function(changes, connections, drops) {
+    # identify changes by id by round
+    return(mapply(function(x, y, z) {
+        if(nrow(x) > 0) {
+            start <- build_start_frame(y)
+            round_delta <- build_round_change_frame(x)
+
+            for(i in 1:max(round_delta$Group.1)) {
+                tmp <- subset(round_delta, Group.1 == i)
+                if(i == 1) {
+                    res <- merge(start,
+                                 tmp[, c('Group.2', 'conns')],
+                                 by.x = 'Group.1',
+                                 by.y = 'Group.2',
+                                 all = TRUE
+                    )
+                    res[is.na(res)] <- 0
+                    names(res) <- c('id', 'start', 'rd1')
+                    res$rd1 <- res$start + res$rd1
+                } else {
+                    res <- merge(res,
+                                 tmp[, c('Group.2', 'conns')],
+                                 by.x = 'id',
+                                 by.y = 'Group.2',
+                                 all = TRUE
+                    )
+                    res[is.na(res)] <- 0
+                    names(res)[grep('conns', names(res))] <- paste0('rd', i)
+                    res[, paste0('rd', i)] <- (res[, paste0('rd', (i - 1))] +
+                                               res[, paste0('rd', i)])
+                }
+                res <- identify_drops(x, y, z, res, i)
+            }
+            res <- melt(res, id.vars = 'id')
+            res$variable <- suppressWarnings(ifelse(res$variable == 'start', 1,
+                                   as.numeric(gsub('rd', '', res$variable)) + 1))
+            names(res) <- c('pid', 'round', 'num_neighbors')
+        } else {
+            res <- data.frame()
+        }
+        return(res)
+    }, changes, connections, drops, SIMPLIFY = FALSE))
+}
+
+clean_rewiring_connections <- function(changes, conns) {
+    res <- mapply(function(x, y) {
+        x$action_num <- ifelse(x$action == 'maintainConnection', 0,
+                               ifelse(x$action == 'breakConnection', -1, 1))
+        if(nrow(x) > 0) {
+            tmp <- do.call(rbind, lapply(split(x, x$round), function(y) {
+                y$alt_action_num <- 0
+                y$redundant <- 0
+                for(i in 1:nrow(y)) {
+                    nid <- y$nid[i]
+                    pid <- y$pid[i]
+                    alt <- which(y$nid == pid & y$pid == nid)
+                    if(length(alt) > 0) {
+                        # to deal with the e1.2 data on 24 aug 2017
+                        if(length(alt) == 2) {
+                            y <- y[-alt[2], ]
+                            alt <- which(y$nid == pid & y$pid == nid)
+                        }
+                        if(alt > i) {
+                            y$alt_action_num[i] <- y$action_num[alt]
+                            y$redundant[alt] <- 1
+                        }
+                    }
+                }
+                y$overall_action <- ifelse(
+                    y$action_num == 1 & y$alt_action_num == 1, 1,
+                    ifelse(y$action_num == -1 | y$alt_action_num == -1, -1, 0))
+                return(y)
+            }))
+        } else {
+            tmp <- data.frame()
+        }
+        return(tmp)
+    }, changes, conns, SIMPLIFY = FALSE)
+    return(res)
+}
+
+create_subsets <- function(data, action, scrubs, strip_practice=TRUE) {
+    # create data subsets
+    return(lapply(data, function(x) {
+        if(action %in% x$event) {
+            tmp <- dcast(subset(x, event == action), id ~ data.name,
+                         value.var = 'data.value')
+            if(strip_practice) {
+                tmp$round <- suppressWarnings(as.numeric(tmp$round))
+                tmp <- subset(tmp, !is.na(round))
+            }
+            return(tmp[complete.cases(tmp), ])
         } else {
             return(data.frame())
         }
     }))
 }
 
-changes_by_round <- function(changes, connections) {
-    # identify changes by id by round
+experiments_to_drop <- function(d) {
+    bad_cond <- unique(d$session[d$condition == 'Other'])
+    no_ppl <- aggregate(d$pid[d$round == 1], by = list(d$session[d$round == 1]),
+                        length)
+    no_ppl <- no_ppl$Group.1[no_ppl$x < 8]
+    no_rds <- aggregate(d$round, by = list(d$session), function(x) {
+        length(unique(x))
+    })
+    no_rds <- no_rds$Group.1[no_rds$x < 2]
+    return(unique(c(bad_cond, no_ppl, no_rds)))
+}
+
+find_dropped_ids <- function(d) {
+    res <- lapply(d, function(x) {
+        tmp <- data.frame()
+        alive <- unique(x$pid[x$round == 1])
+        for(i in 2:max(x$round)) {
+            dead <- subset(alive, !(alive %in% unique(x$pid[x$round == i])))
+            if(length(dead) > 0) {
+                tmp <- rbind(tmp, data.frame(rd = (i - 1), drop = dead,
+                                             stringsAsFactors = FALSE))
+                alive <- alive[which(alive %in% unique(x$pid[x$round == i]))]
+            }
+        }
+        return(tmp)
+    })
+    return(res)
+}
+
+get_neighbor_number <- function(d, cnx, rd) {
+    ids <- d$pid[d$round == rd]
+    tmp <- cnx[cnx$playerId1 %in% ids & cnx$playerId2 %in% ids, ]
+    tmp_df <- merge(
+        data.frame(table(tmp$playerId1)),
+        data.frame(table(tmp$playerId2)),
+        by = 'Var1',
+        all = TRUE
+    )
+    tmp_df[is.na(tmp_df)] <- 0
+    for(id in ids) {
+        d$num_neighbors[d$round == rd & d$pid == id] <- sum(
+            tmp_df$Freq.x[tmp_df$Var1 == id],
+            tmp_df$Freq.y[tmp_df$Var1 == id]
+        )
+    }
+    return(d)
+}
+
+identify_drops <- function(changes, connections, drops, results, iter) {
+    # determine if anyone dropped
+    if(nrow(drops[drops$rd == iter, ]) > 0) {
+        coffin <- drops$drop[drops$rd == iter]
+        graveyard <- changes[which(changes$round == iter &
+                                   (changes$pid %in% coffin |
+                                    changes$nid %in% coffin) &
+                                   changes$overall_action >= 0),
+                             c('nid', 'pid', 'round', 'overall_action')]
+        doornails <- changes[which(changes$round == iter &
+                                   (changes$pid %in% coffin |
+                                    changes$nid %in% coffin) &
+                                   changes$overall_action == -1),
+                             c('nid', 'pid', 'round', 'overall_action')]
+        graveyard$connected <- NA
+        for(j in 1:nrow(graveyard)) {
+            p1 <- graveyard$nid[j]
+            p2 <- graveyard$pid[j]
+            cnx <- which((connections$playerId1 == p1 & connections$playerId2 == p2) |
+                         (connections$playerId1 == p2 & connections$playerId2 == p1))
+            if(length(cnx) > 0) {
+                graveyard$connected[j] <- 1
+            }
+        }
+        graveyard <- subset(graveyard, overall_action == 1 | connected == 1)
+        for(j in 1:nrow(doornails)) {
+            buried <- which(graveyard$nid == doornails$nid[j] &
+                            graveyard$pid == doornails$pid[j])
+            if(length(buried) > 0) {
+                graveyard <- graveyard[-buried, ]
+            }
+        }
+        lost_conns <- c(graveyard$nid, graveyard$pid)[!c(graveyard$nid,
+                                                         graveyard$pid) %in% coffin]
+        if(length(lost_conns) > 0) {
+            lost_conns <- aggregate(lost_conns, by = list(lost_conns), length)
+            results <- merge(results, lost_conns, by.x = 'id', by.y = 'Group.1',
+                             all = TRUE)
+            results[is.na(results)] <- 0
+            results[, paste0('rd', iter)] <- results[, paste0('rd', iter)] - results$x
+            results <- results[, grep('^id|sta|rd', names(results))]
+            results[results$id %in% coffin, paste0('rd', iter)] <- 0
+        }
+    }
+    return(results)
+}
+
+merge_with_empanelment <- function(data, empanel, merge_var, rename = FALSE,
+                                   rename_var = NULL) {
+    tmp <- merge(data, empanel, by.x = merge_var, by.y = 'bbid', all.x = TRUE)
+    if(rename) {
+        names(tmp)[which(names(tmp) %in% 'empanel_id')] <- rename_var
+    }
+    return(tmp)
+}
+
+read_input_files <- function(dir) {
+    # identifies all csv files in a directory, reads then into r, and appends them
+    res <- list()
+    files <- list.files(dir, pattern = '.csv$')
+    sources <- strsplit(files, split = '_')
+    for(i in 1:length(files)) {
+        tmp <- read.csv(paste(dir, files[i], sep = '/'), header = TRUE,
+                        sep = ',', stringsAsFactors = FALSE)
+        tmp$source <- paste(sources[[i]][1:2], collapse = '_')
+        res[[i]] <- tmp
+        names(res)[i] <- paste(sources[[i]][1:2], collapse = '_')
+    }
+    return(res)
+}
+
+rewiring_round_changes <- function(changes, connections) {
+    # a list-impleentation for identifying changes by id by round
     return(mapply(function(x, y) {
         round_changes <- list()
         if(nrow(x) > 0) {
@@ -251,78 +465,6 @@ changes_by_round <- function(changes, connections) {
     }, changes, connections, SIMPLIFY = FALSE))
 }
 
-create_subsets <- function(data, action, scrubs, strip_practice=TRUE) {
-    # create data subsets
-    return(lapply(data, function(x) {
-        if(action %in% x$event) {
-            tmp <- dcast(subset(x, event == action), id ~ data.name,
-                         value.var = 'data.value')
-            if(strip_practice) {
-                tmp$round <- as.numeric(tmp$round)
-                tmp <- subset(tmp, !is.na(round))
-            }
-            return(tmp[complete.cases(tmp), ])
-        } else {
-            return(data.frame())
-        }
-    }))
-}
-
-experiments_to_drop <- function(d) {
-    bad_cond <- unique(d$session[d$condition == 'Other'])
-    no_ppl <- aggregate(d$pid[d$round == 1], by = list(d$session[d$round == 1]),
-                        length)
-    no_ppl <- no_ppl$Group.1[no_ppl$x < 8]
-    no_rds <- aggregate(d$round, by = list(d$session), function(x) {
-        length(unique(x))
-    })
-    no_rds <- no_rds$Group.1[no_rds$x < 2]
-    return(unique(c(bad_cond, no_ppl, no_rds)))
-}
-
-get_neighbor_number <- function(d, cnx, rd) {
-    ids <- d$pid[d$round == rd]
-    tmp <- cnx[cnx$playerId1 %in% ids & cnx$playerId2 %in% ids, ]
-    tmp_df <- merge(
-        data.frame(table(tmp$playerId1)),
-        data.frame(table(tmp$playerId2)),
-        by = 'Var1',
-        all = TRUE
-    )
-    tmp_df[is.na(tmp_df)] <- 0
-    for(id in ids) {
-        d$num_neighbors[d$round == rd & d$pid == id] <- sum(
-            tmp_df$Freq.x[tmp_df$Var1 == id],
-            tmp_df$Freq.y[tmp_df$Var1 == id]
-        )
-    }
-    return(d)
-}
-
-merge_with_empanelment <- function(data, empanel, merge_var, rename = FALSE,
-                                   rename_var = NULL) {
-    tmp <- merge(data, empanel, by.x = merge_var, by.y = 'bbid', all.x = TRUE)
-    if(rename) {
-        names(tmp)[which(names(tmp) %in% 'empanel_id')] <- rename_var
-    }
-    return(tmp)
-}
-
-read_input_files <- function(dir) {
-    # identifies all csv files in a directory, reads then into r, and appends them
-    res <- list()
-    files <- list.files(dir, pattern = '.csv$')
-    sources <- strsplit(files, split = '_')
-    for(i in 1:length(files)) {
-        tmp <- read.csv(paste(dir, files[i], sep = '/'), header = TRUE,
-                        sep = ',', stringsAsFactors = FALSE)
-        tmp$source <- paste(sources[[i]][1:2], collapse = '_')
-        res[[i]] <- tmp
-        names(res)[i] <- paste(sources[[i]][1:2], collapse = '_')
-    }
-    return(res)
-}
-
 # Load raw breadboard data
 exp1 <- read_input_files(paste0('NGS2-Cycle', CYCLE, '-Experiment1/data'))
 
@@ -337,20 +479,18 @@ act <- create_subsets(exp1, 'cooperationEvent', 'pid|round',
                       strip_practice = TRUE)
 chng <- create_subsets(exp1, 'rewiringEvent', 'pid|nid|round',
                        strip_practice = TRUE)
-chng <- lapply(chng, function(x) {
-    if(nrow(x) > 0) {
-        x$action_num <- ifelse(x$action == 'maintainConnection', 0,
-                               ifelse(x$action == 'breakConnection', -1, 1))
-    }
-    return(x)
-})
+
+# clean up changes dataset to ensure proper accounting of neighbors
+chng_clean <- clean_rewiring_connections(chng, conn)
 
 # start building the `cooperation` output file
 coop <- build_cooperation(act)
 
+# identify when players drop from experiment
+dead <- find_dropped_ids(coop)
+
 # calculate actor moves by round
-round_changes <- changes_by_round(chng, conn)
-nbr_connections_round <- calculate_connections(round_changes)
+nbr_connections_round <- changes_by_round(chng_clean, conn, dead)
 
 # finish off the `cooperation` dataset
 coop <- mapply(function(x, y, exp) {
@@ -395,13 +535,13 @@ coop <- subset(coop, !(session %in% experiment_drops))
 # output dataset to disk
 write.csv(coop[order(coop$session, coop$round, coop$pid), COOP_VAR_ORDER],
           file = 'NGS2-Cycle1-Experiment1/cooperation_exp1.csv',
-          row.names = FALSE, na='')
+          row.names = FALSE, na = '')
 
 # rewire dataset
 # start building `rewire` output file
-rewire <- build_rewire(chng)
+rewire <- build_rewire(chng_clean)
 
-# add respondent actions to `rewire` dataset
+# # add respondent actions to `rewire` dataset
 rewire <- add_actions(rewire, act, focus = 'player')
 rewire <- add_actions(rewire, act, focus = 'alter')
 
@@ -420,7 +560,8 @@ rewire <- lapply(rewire, function(x) {
 })
 
 # add in previous and current ties
-rewire <- add_ties(rewire, conn, round_changes)
+rewire_round_changes <- rewiring_round_changes(chng_clean, conn)
+rewire <- add_ties(rewire, conn, rewire_round_changes)
 
 # add oin parameters from dataset for completeness
 rewire <- do.call(rbind, mapply(function(x, name) {

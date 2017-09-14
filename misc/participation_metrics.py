3ab4105108391545a975a846b446097377000985
Matt Hoover <matthew.a.hoover@gmail.com>
2017-09-14 18:33:13 -0400

    Add an output that details by experiment/condition.

--- misc/participation_metrics.py
+++ misc/participation_metrics.py
@@ -4,13 +4,57 @@ import os
 import pandas as pd
 
 
-def create_output_tables(data, experiment):
-    return pd.DataFrame({
-        'started': [data.id.count()],
-        'finished': [data.final_score.count()],
-        'total_experiments': [len(set(data.experiment))],
-        'experiment_nbr': [experiment]
-    })
+VARORDER = [
+    'experiment_nbr',
+    'started',
+    'finished',
+    'total_experiments',
+]
+VARORDER_DETAIL = [
+    'experiment_nbr',
+    'started',
+    'finished',
+    'total_experiments',
+    'condition',
+]
+
+
+def create_output_tables(data, proc, experiment, detail=False):
+    def experiment_number(x):
+        return len(set(x))
+
+    def modal_condition(x):
+        return max(set(x))
+
+    proc['experiment_nbr'] = proc.session.apply(lambda x: x.split('_')[0])
+    condition = (proc
+                 .groupby('experiment_nbr')
+                 .agg({'condition': modal_condition})
+                 .reset_index()
+    )
+
+    if detail:
+        data['exp_num'] = data.experiment.apply(lambda x: x.split('_')[0])
+        table = data.groupby('exp_num').agg({
+            'id': 'count',
+            'final_score': 'count',
+            'experiment': experiment_number,
+        }).reset_index()
+        table.rename(columns={
+            'id': 'started',
+            'final_score': 'finished',
+            'exp_num': 'experiment_nbr',
+            'experiment': 'total_experiments',
+        }, inplace=True)
+        table = table.merge(condition, on='experiment_nbr')
+        return table
+    else:
+        return pd.DataFrame({
+            'started': [data.id.count()],
+            'finished': [data.final_score.count()],
+            'total_experiments': [len(set(data.experiment))],
+            'experiment_nbr': [experiment],
+        })
 
 
 def gather_data(directory, file, experiment):
@@ -54,20 +98,29 @@ def process_directory(directory, experiment):
 def run():
     # load data
     data = [process_directory(directory, experiment) for directory, experiment in
-            zip(FOLDERS, EXPERIMENTS)]
+            zip(RAW_DATA, EXPERIMENTS)]
+    proc = [pd.read_csv(file, sep=None, engine='python') for file in PROCESSED_DATA]
+
 
     # build output tables
-    table = pd.concat([create_output_tables(d, exp) for d, exp in
-                       zip(data, EXPERIMENTS)])
+    table = pd.concat([create_output_tables(d, p, exp, False) for
+                      d, p, exp in zip(data, proc, EXPERIMENTS)])
+    table_detail = pd.concat([create_output_tables(d, p, exp, True) for
+                             d, p, exp in zip(data, proc, EXPERIMENTS)])
 
     # output table
-    table.to_csv('misc/current_experiment_statistics.csv', index=False)
-
+    table[VARORDER].to_csv('misc/current_experiment_statistics.csv', index=False)
+    table_detail[VARORDER_DETAIL].to_csv('misc/current_experiment_statistics_detail.csv',
+                                         index=False)
 
 if __name__ == '__main__':
-    FOLDERS = [
+    RAW_DATA = [
         'NGS2-Cycle1-Experiment1/data',
         'NGS2-Cycle1-Experiment2/data',
+    ]
+    PROCESSED_DATA = [
+        'NGS2-Cycle1-Experiment1/cooperation_exp1.csv',
+        'NGS2-Cycle1-Experiment2/cooperation_exp2.csv',
     ]
     EXPERIMENTS = [
         1,

6b3fa13fb9d8d181736c5b3238eaf7ad72d77209
Matt Hoover <matthew.a.hoover@gmail.com>
2017-09-18 16:10:58 -0400

    Finalize bash program.

--- misc/make_metrics.sh
+++ misc/make_metrics.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
+
 # switch to right directory
 cd /Users/matt_hoover/git/NGS2
-
-# check to ensure on the right branch; this can go away once merged
-repo=$(git symbolic-ref --short -q HEAD)
-if [ $repo != 'batch-eligibility-identification' ]
-    then
-        git stash
-        echo "NOTICE! `git stash`'d changes; be aware."
-
-        git co batch-eligibility-identification
-fi
 
 # identify the number of breadboard files
 of=$(cat misc/nfiles)
@@ -18,20 +9,25 @@ e1=$(ls NGS2-Cycle1-Experiment1/data | wc -l)
 e2=$(ls NGS2-Cycle1-Experiment2/data | wc -l)
 nf=$(echo $e1 + $e2 | bc)
 
-# run participation metrics script
+# check/run participation metrics script
 if [ $of != $nf ]
     then
+        # execute python program to calculate tallies
         python misc/participation_metrics.py
 
-    # grab files and email them to others
-    DATE=$(date '+\%Y-\%m-\%d')
-    printf "The latest participation numbers from $DATE attached. \
-            Questions? Email matt_hoover@gallup.com." |
-    uuencode misc/current* |
-    mail -s "NGS2 Metrics: Updated participation numbers" \
-         matt_hoover@gallup.com
+        # grab files and email them to others
+        DATE=$(date '+%Y-%m-%d')
+        BODY=$(echo "The latest participation numbers from $DATE attached. Email matt_hoover@gallup.com with questions.")
+        (echo $BODY; uuencode misc/current_experiment_statistics_detail.csv current_experiment_statistics.csv) |
+        mailx -s "NGS2 Metrics: Updated participation numbers" \
+            matt_hoover@gallup.com \
+            dan_foy@gallup.com \
+            sofia_kluch@gallup.com \
+            c_pablo_diego-rosell@gallup.com \
+            anuradha_uduwage@gallup.com \
+            mark.mcknight@yale.edu \
+            zach_bikus@gallup.com
 
-    # update the `nfile` number
-    echo $nf > misc/nfiles
+        # update the `nfile` number
+        echo $nf > misc/nfiles
 fi
-

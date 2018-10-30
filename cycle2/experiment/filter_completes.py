8f834fd119f0b14fb2e920c8ecc1fc2167667a3a
Matthew Hoover <matt_hoover@gallup.com>
2018-10-30 17:33:32 -0400

    Add script to filter out completers from next round of experiment invites.

---
+++ cycle2/experiment/filter_completes.py
@@ -0,0 +1,31 @@
+#!/usr/local/Cellar/python/3.6.5/bin
+# -*- coding: utf-8 -*-
+import argparse
+import os
+import pandas as pd
+
+
+def run(args_dict):
+    # read in data
+    emp = pd.read_csv(args_dict['empanel'], sep=None, engine='python')
+    ids = pd.read_csv(args_dict['ids'], sep=None, engine='python')
+
+    # manipulate data
+
+
+    # write data to disk
+    fpath = os.path.splitext(args_dict['empanel'])
+    emp.to_csv('{}_filtered{}'.format(fpath[0], fpath[1]), index=False)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Filter out previous '
+                                     'completes before processing newly '
+                                     'empaneled participants.')
+    parser.add_argument('-e', '--empanel', required=True, help='Path/name to '
+                        'new empanelment data.')
+    parser.add_argument('-i', '--ids', required=True, help='IDs of people who '
+                        'have played before.')
+    args_dict = vars(parser.parse_args())
+
+    run(args_dict)

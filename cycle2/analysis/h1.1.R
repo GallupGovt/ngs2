60d40202b73219243661cfa7d9960a45e87fe218
Pablo Diego-Rosell <pablo_diego-rosell@gallup.com>
2018-11-19 14:21:52 +0100

    Update h1.1.R
    Set autoscale = FALSE for all prior distributions

--- cycle2/analysis/h1.1.R
+++ cycle2/analysis/h1.1.R
@@ -9,7 +9,7 @@ nCoef1.1<-nCoef-4
 # Prediction 1
 # Set priors according to prediction 1: No comp (ref), Lo comp (-1.45), Med comp (1.45), Hi comp (-1.45)
 test_prior <- cauchy(location = c(logodds$h1.1.locomp,logodds$h1.1.medcomp,logodds$h1.1.hicomp,rep(0,nCoef1.1)), 
-                     scale = c(test.SD,test.SD,test.SD,rep(2.5,nCoef1.1)))
+                     scale = c(test.SD,test.SD,test.SD,rep(2.5,nCoef1.1)), autoscale = FALSE)
 # Update posterior model parameters via bayesGlmer function, based on priors for Prediction 1
 glmm1.1.test<- bayesGlmer(main.formula, test_prior)
 # Estimate marginal log likelihood of Prediction 1 given data via bridge_sampler
@@ -18,7 +18,7 @@ bridge_1.1.test <- bridge_sampler(glmm1.1.test)
 # Prediction 2
 # Set priors according to prediction 2: No comp (ref), Lo comp (0.00), Med comp (0.00), Hi comp (0.00)
 null_prior <- cauchy(location = 0, 
-                     scale = c(test.SD,test.SD,test.SD,rep(2.5,nCoef1.1)))
+                     scale = c(test.SD,test.SD,test.SD,rep(2.5,nCoef1.1)), autoscale = FALSE)
 
 # Update posterior model parameters via bayesGlmer function, based on priors for Prediction 2
 glmm1.1.null<- bayesGlmer(main.formula, null_prior)
@@ -35,7 +35,7 @@ testnullBF<-testnull$bf
 # Prediction 3
 # Set priors according to prediction 3: No comp (ref), Lo comp (-1.45), Med comp (0.00), Hi comp (1.45)
 alt_prior <- cauchy(location = c(logodds$h1.1.locomp,0, logodds$h1.1.medcomp,rep(0,nCoef1.1)), 
-                    scale = c(test.SD,test.SD,test.SD,rep(2.5,nCoef1.1)))
+                    scale = c(test.SD,test.SD,test.SD,rep(2.5,nCoef1.1)), autoscale = FALSE)
 
 # Update posterior model parameters via bayesGlmer function, based on priors for Prediction 3
 glmm1.1.alt<- bayesGlmer(main.formula, alt_prior)

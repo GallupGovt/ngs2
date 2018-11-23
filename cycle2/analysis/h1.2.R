d6107a3e8c5bf3d85fdda1e291fe3eddbb0d57d4
Pablo Diego-Rosell <pablo_diego-rosell@gallup.com>
2018-11-23 14:18:05 +0100

    Update h1.2.R
    Add frame to store Bayes factors

--- cycle2/analysis/h1.2.R
+++ cycle2/analysis/h1.2.R
@@ -71,3 +71,12 @@ alt1alt2.1.2<-bf(bridge_1.2.alt1, bridge_1.2.alt2)$bf
 testnull.1.2<-bf(bridge_1.2.test, bridge_1.2.null)$bf
 alt1null.1.2<-bf(bridge_1.2.alt1, bridge_1.2.null)$bf
 alt2null.1.2<-bf(bridge_1.2.alt2, bridge_1.2.null)$bf
+
+BFs1.2 <- data.frame(1.2, testalt1.1.2, testalt2.1.2, alt1alt2.1.2, testnull.1.2, alt1null.1.2, alt2null.1.2)
+colnames(BFs) <- c("Hypothesis", 
+                   "Prediction 1 vs. Prediction 2", 
+                   "Prediction 1 vs. Prediction 3", 
+                   "Prediction 2 vs. Prediction 3", 
+                   "Prediction 1 vs. Null", 
+                   "Prediction 2 vs. Null", 
+                   "Prediction 3 vs. Null")

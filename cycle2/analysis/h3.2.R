38996445f2e58b867b21793953e2a8dc0c965c6b
Pablo Diego-Rosell <pablo_diego-rosell@gallup.com>
2018-11-26 15:16:23 +0100

    Add files via upload
    Scripts to test hypothesis 3.2

---
+++ cycle2/analysis/h3.2.R
@@ -0,0 +1,69 @@
+## Created by Pablo Diego Rosell, PhD, for Gallup inc. in September 2018
+## For any questions, contact pablo_diego-rosell@gallup.co.uk
+
+# Test of Hypothesis 3.2 and all associated predictions
+# Manually set priors for all h3.2. predictions
+
+ndim.3.2 <- nCoef-9
+# Assume SD = half of a small effect
+
+test.SD<-log.odds.large/2
+
+# Null hypothesis: Leader tolerance of ambiguity does not affect matchid motivation to innovate.
+
+h3.2.null <- cauchy(location = 0, 
+                    scale = c(rep(2.5,7), test.SD, rep(2.5,ndim.3.2)), autoscale = FALSE)
+
+# Test hypothesis: Average levels of tolerance of ambiguity in a group will increase motivation to innovate.
+
+
+h3.2.test <- cauchy(location = c(rep(0,7), log.odds.large, rep(0,ndim.3.2)), 
+                    scale = c(rep(2.5,7), test.SD, rep(2.5,ndim.3.2)), autoscale = FALSE)
+
+# Alternative hypothesis: Groups with leaders high in TA will be less willing to innovate as the game progresses.
+# Leader TA interacts with round number (new formula required)
+
+factorial$roundCent <- (factorial$round-7)
+
+h3.2alt.formula <- innovation~h1.1+h1.3+h2.1+h3.1+h3.2*roundCent+
+  h3.3+h3.4+h3.5+tools+(1|matchid)
+
+coefficients.h3.2alt <- stan_glmer(h3.2alt.formula, data=factorial, family = binomial(link = "logit"), 
+                                   chains = 1, iter = 100)
+
+# Identify location of relevant coefficients for alternative formula
+
+ndim.3.2alt <- length(coefficients.h3.2alt$prior.info$prior$location)
+
+h3.2.alt1 <- cauchy(location = c(rep(0,ndim.3.2alt-1), log.odds.large), 
+                    scale = c(rep(2.5,ndim.3.2-1), test.SD), autoscale = FALSE)
+
+# Estimate and save all models
+
+glmm3.2.test <- bayesGlmer(main.formula, h3.2.test)
+glmm3.2.null <- bayesGlmer(main.formula, h3.2.null)
+glmm3.2.alt1 <- bayesGlmer(h3.2alt.formula, h3.2.alt1)
+
+# Estimate marginal likelihood
+
+bridge_3.2.null <- bridge_sampler(glmm3.2.null)
+bridge_3.2.test <- bridge_sampler(glmm3.2.test)
+bridge_3.2.alt1 <- bridge_sampler(glmm3.2.alt1)
+
+# Calculate BFs for all comparisons
+
+testalt1.3.2<-bf(bridge_3.2.test, bridge_3.2.alt1)$bf
+testnull.3.2<-bf(bridge_3.2.test, bridge_3.2.null)$bf
+alt1null.3.2<-bf(bridge_3.2.alt1, bridge_3.2.null)$bf
+
+# Store BFs
+
+BFs3.2 <- data.frame(3.2, testalt1.3.2, NA, NA, testnull.3.2, alt1null.3.2, NA)
+colnames(BFs) <- c("Hypothesis", 
+                   "Prediction 1 vs. Prediction 2", 
+                   "Prediction 1 vs. Prediction 3", 
+                   "Prediction 2 vs. Prediction 3", 
+                   "Prediction 1 vs. Null", 
+                   "Prediction 2 vs. Null", 
+                   "Prediction 3 vs. Null")
+

bcc42c639d57216ec647372b218764b56622b940
Pablo Diego-Rosell <pablo_diego-rosell@gallup.com>
2018-11-29 12:32:09 +0100

    Update analytics.R
    Reduce y axis for bayesPlotter

--- cycle2/analysis/analytics.R
+++ cycle2/analysis/analytics.R
@@ -75,11 +75,11 @@ frame.posterior<-subset(plotdf, Distribution=="Posterior")
 postPlot<-ggplot(frame.posterior, aes(value, fill=Level, linetype=Distribution)) + 
   geom_density(alpha=0.4) + 
   scale_x_continuous(limits = c(-5, 5)) + 
-  scale_y_continuous(limits = c(0, 15))
+  scale_y_continuous(limits = c(0, 5))
 bfPlot<-ggplot(plotdf, aes(value, fill=Level, linetype=Distribution)) + 
   geom_density(alpha=0.4) + 
   scale_x_continuous(limits = c(-5, 5)) + 
-  scale_y_continuous(limits = c(0, 15)) +
+  scale_y_continuous(limits = c(0, 5)) +
   annotate("text", x=2, y=1.7, label = paste(deparse(substitute(plotBF)), " = ", sprintf("%0.2f", plotBF))) +
   geom_vline(xintercept = 0, linetype="dashed")
 return(list(postPlot, bfPlot))

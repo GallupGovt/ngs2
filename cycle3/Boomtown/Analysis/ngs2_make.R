951595c4059c1a66fd9c4fcf006ebbc34903f4b1
Pablo Diego-Rosell <pablo_diego-rosell@gallup.com>
2019-09-13 00:15:17 +0200

    Add files via upload

---
+++ cycle3/Boomtown/Analysis/ngs2_make.R
@@ -0,0 +1,9 @@
+# Make report using Docker
+
+library(lubridate)
+library(ggplot2)
+library(data.table)
+library(rmarkdown)
+
+# Make the report
+render('NGS2_WITNESS_Cycle3_Prereg.RMD', output_file='NGS2_WITNESS_Cycle3_Prereg.html')

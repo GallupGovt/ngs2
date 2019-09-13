07951244f4733446dd872e34cf0e0cad2d1fa700
Pablo Diego-Rosell <pablo_diego-rosell@gallup.com>
2019-09-13 16:42:10 +0200

    Add files via upload

---
+++ cycle3/Boomtown/Analysis/analytics.R
@@ -0,0 +1,16 @@
+## Created by Pablo Diego Rosell, PhD, for Gallup inc. in September 2019
+## Bayesian hypothesis testing (Individual-level outcomes)
+
+# Number of valid games
+
+nGames<-length(unique(factorial$group))
+
+# Number of players connected
+
+nPlayers<-length(unique(factorial$player))
+
+# Number of unique experimental conditions played
+
+#factorial$settingsNum<-as.numeric(levels(factorial$settingsNum))[factorial$settingsNum]
+#nConditions<-length(unique(factorial$settingsNum))
+

e66999d04fc7ccb1c7b78809c72279b41a53307c
Pablo Diego-Rosell <pablo_diego-rosell@gallup.com>
2019-09-17 11:39:06 +0200

    Update functions.R

--- cycle3/Boomtown/Analysis/functions.R
+++ cycle3/Boomtown/Analysis/functions.R
@@ -1,5 +1,4 @@
 ## Created by Pablo Diego Rosell, PhD, for Gallup inc. in June 2019
-
 # For questions, please email pablo_diego-rosell@gallup.co.uk
 
 bayesGlmer<-function(formula, priors, dataset = factorial) {
@@ -7,13 +6,14 @@ bayesGlmer<-function(formula, priors, dataset = factorial) {
   formulatext <- gsub("formula.", "",deparse(substitute(formula)))
   priorstext <- deparse(substitute(priors))
   label<-paste("bridge_",formulatext, "_", priorstext, sep="")
+  diagnostic<-paste("diagnostic_",formulatext, "_", priorstext, ".csv", sep="")
   fittedGlmer<- stan_glmer(formula,
                            data=dataset,
                            family = binomial(link = "logit"),
                            prior = priors,
                            prior_intercept = normal(0, 2.5),
                            chains = 3, iter = nIter,
-                           diagnostic_file = "diagnostics.csv")
+                           diagnostic_file = diagnostic
   save (fittedGlmer, file = paste("bayesGlmer.",label, sep=""))
   bridge_priors <- bridge_sampler(fittedGlmer, silent=TRUE)
   save (bridge_priors, file = paste("bridge_",label, sep=""))
@@ -25,16 +25,16 @@ bayesLmer<-function(formula, priors, dataset = factorial) {
   formulatext <- gsub("formula.", "",deparse(substitute(formula)))
   priorstext <- deparse(substitute(priors))
   label<-paste("bridge_",formulatext, "_", priorstext, sep="")
+  diagnostic<-paste("diagnostic_",formulatext, "_", priorstext, ".csv", sep="")
   fittedLmer<- stan_glmer(formula,
                            data=dataset,
                            family = gaussian(link = "identity"),
                            prior = priors,
                            prior_intercept = normal(0, 2.5),
                            chains = 3, iter = nIter,
-                           diagnostic_file = "diagnostics.csv")
+                           diagnostic_file = diagnostic)
   save (fittedLmer, file = paste("bayesLmer.",label, sep=""))
-  bridge_priors <- bridge_sampler(fittedGlmer, silent=TRUE)
+  bridge_priors <- bridge_sampler(fittedLmer, silent=TRUE)
   save (bridge_priors, file = paste("bridge_",label, sep=""))
   return(bridge_priors)
 }
-

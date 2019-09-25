d650f5f5d1169e6138e4843d0e12b6e27d357274
Pablo Diego-Rosell <pablo_diego-rosell@gallup.com>
2019-09-25 07:40:40 +0200

    Update functions.R

--- cycle3/Boomtown/Analysis/functions.R
+++ cycle3/Boomtown/Analysis/functions.R
@@ -40,3 +40,59 @@ bayesLmer<-function(formula, priors, dataset = factorial) {
   save (bridge_priors, file = paste("bridge_",label, sep=""))
   return(bridge_priors)
 }
+
+# Bayesian plotting functions
+
+modelPlotter<- function (model, ivs) {
+  posterior <- as.array(model)
+  mcmc_areas(posterior, pars = ivs, 
+             prob = 0.8, # 80% intervals
+             prob_outer = 0.99, # 99%
+             point_est = "mean")
+}
+
+bayesPlotter <- function (plotdf, plotBF) {
+  frame.prior<-subset(plotdf, Distribution=="Prior")
+  priorPlot<-ggplot(frame.prior, aes(value, fill=Level, linetype=Distribution)) + 
+    geom_density(alpha=0.4) + 
+    scale_x_continuous(limits = c(-1.5, 1.5)) + 
+    scale_y_continuous(limits = c(0, 5)) +
+    annotate("text", x=2, y=1.7, label = paste(deparse(substitute(plotBF)), " = ", sprintf("%0.2f", plotBF))) +
+    geom_vline(xintercept = 0, linetype="dashed")
+  frame.posterior<-subset(plotdf, Distribution=="Posterior")
+  postPlot<-ggplot(frame.posterior, aes(value, fill=Level, linetype=Distribution)) + 
+    geom_density(alpha=0.4) + 
+    scale_x_continuous(limits = c(-1.5, 1.5)) + 
+    scale_y_continuous(limits = c(0, 5))
+  bfPlot<-ggplot(plotdf, aes(value, fill=Level, linetype=Distribution)) + 
+    geom_density(alpha=0.4) + 
+    scale_x_continuous(limits = c(-1.5, 1.5)) + 
+    scale_y_continuous(limits = c(0, 5)) +
+    annotate("text", x=2, y=1.7, label = paste(deparse(substitute(plotBF)), " = ", sprintf("%0.2f", plotBF))) +
+    geom_vline(xintercept = 0, linetype="dashed")
+  return(list(priorPlot, postPlot, bfPlot))
+}
+
+# Bayesian plotting - frame processing function (Predictions with three coefficients)
+
+bayesPlotter3 <- function (model, priorList, priors1, priors2, priors3, priorScale, coef1, coef2, coef3, plotBF) {
+  plotIters<-nIter*1.5  
+  draws <- as.data.frame(model)
+  a <- rnorm(plotIters, mean=priorList[[priors1]], sd=priorScale)
+  b <- rnorm(plotIters, mean=priorList[[priors2]], sd=priorScale)
+  c <- rnorm(plotIters, mean=priorList[[priors3]], sd=priorScale)
+  d <- draws[[coef1]]
+  e <- draws[[coef2]]
+  f <- draws[[coef3]]
+  plotdf <- data.frame(value=c(a, b, c, d, e, f), 
+                       Distribution=c(rep("Prior", plotIters*3),
+                                      rep("Posterior", plotIters*3)), 
+                       Level=c(rep(priors1, plotIters),
+                               rep(priors2, plotIters),
+                               rep(priors3, plotIters), 
+                               rep(priors1, plotIters),
+                               rep(priors2, plotIters),
+                               rep(priors3, plotIters)))
+  plots<-bayesPlotter(plotdf, plotBF)
+  return(plots)
+}

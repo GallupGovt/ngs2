176862ffcd4579a44c9aabf3d4f6ce46b5ecf75a
Pablo Diego-Rosell <pablo_diego-rosell@gallup.com>
2019-10-16 17:08:17 +0200

    Delete libraries.R

--- cycle3/Boomtown/Analysis/libraries.R
+++
@@ -1,7 +0,0 @@
-## Created by Pablo Diego Rosell, PhD, for Gallup inc. in October 2019
-
-knitr::opts_chunk$set(echo = FALSE, warning = FALSE, strip.white=TRUE, tidy=TRUE)
-# load libraries
-if (!require("pacman")) install.packages("pacman")
-library ("pacman")
-pacman::p_load(rstan, rstanarm, ggplot2, Hmisc, httr, bridgesampling, DT, dplyr, bayesplot, knitr, lme4, RCurl, formatR, caret, pROC, library, formatR, foreach, doParallel)

a09f21eb90abc8f8bcc1ff6c0f980908d24704a7
Pablo Diego-Rosell <pablo_diego-rosell@gallup.com>
2020-03-18 16:19:57 +0100

    Update survey_analytics.R

--- cycle3/Boomtown/Analysis/survey_analytics.R
+++ cycle3/Boomtown/Analysis/survey_analytics.R
@@ -41,7 +41,7 @@ chisq_test_plot <- function(data, key.var, group.var){
     geom_text(aes(label = scales::percent(..prop.., accuracy = 1), y = ..prop..), 
               stat = "count", size = 4, vjust = -0.5, position = position_dodge(0.9)) +
     labs(title = paste0("Comparison of ", gsub("_", " ", key.var), " by ", gsub("_", " ", group.var), " Condition"), 
-         subtitle = paste0("Chi-squre test: X²(", test_summary[1,5],") = ", test_summary[1,4], ", p value = ", test_summary[1,6], "."),
+         subtitle = paste0("Chi-squre test: Chi-Sq (", test_summary[1,5],") = ", test_summary[1,4], ", p value = ", test_summary[1,6], "."),
          x = gsub("_", " ", key.var), 
          y = "Perent (within each group)", 
          fill = paste(gsub("_", " ", group.var), "Condition")) +
@@ -314,7 +314,7 @@ oneway_anova_test <- function(data, key.var, key.var.label = gsub("_", " ", key.
            "One-way ANOVA test: F(", summary(anova)[[1]][[1,"Df"]], ",", summary(anova)[[1]][[2,"Df"]], ") = ", 
            round(summary(anova)[[1]][[1,"F value"]],3),", ", 
            "p value = ", round(summary(anova)[[1]][[1,"Pr(>F)"]], 3), ". \n", 
-           "Kruskal-Wallis test: X²(", kruskal$parameter,") = ", round(kruskal$statistic, 3), ", ", 
+           "Kruskal-Wallis test: XÂ²(", kruskal$parameter,") = ", round(kruskal$statistic, 3), ", ", 
            "p value = ", round(kruskal$p.value, 3), "."),
          x = paste0(group.var.label, " Condition"),  
          y = paste0("Average ", key.var.label, collapse = ), 
@@ -333,7 +333,7 @@ oneway_anova_test <- function(data, key.var, key.var.label = gsub("_", " ", key.
            "One-way ANOVA test: F(", summary(anova)[[1]][[1,"Df"]], ",", summary(anova)[[1]][[2,"Df"]], ") = ", 
            round(summary(anova)[[1]][[1,"F value"]],3),", ", 
            "p value = ", round(summary(anova)[[1]][[1,"Pr(>F)"]], 3), ". \n", 
-           "Kruskal-Wallis test: X²(", kruskal$parameter,") = ", round(kruskal$statistic, 3), ", ", 
+           "Kruskal-Wallis test: XÂ²(", kruskal$parameter,") = ", round(kruskal$statistic, 3), ", ", 
            "p value = ", round(kruskal$p.value, 3), "."),
          x = paste0(group.var.label, " Condition"), 
          y = key.var.label,

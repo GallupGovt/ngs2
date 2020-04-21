2911423ad89b45f2cf79749383ac6ac69c362107
Mitchell Bregman <mitchbregs@gmail.com>
2020-04-21 09:19:24 -0500

    added all latest components

--- cycle3/Boomtown/Analysis/ngs2.R
+++ cycle3/Boomtown/Analysis/ngs2.R
@@ -1,2 +1,2 @@
 library(rmarkdown)
-render('NGS2_WITNESS_Cycle3_Hyp.RMD', output_file='NGS2_WITNESS_Cycle3.html')
+render('NGS2_WITNESS_Cycle3_TEST.rmd', output_file='output/NGS2_WITNESS_Cycle3.html')

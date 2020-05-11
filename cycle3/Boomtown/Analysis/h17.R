fadbf211b3fef947c20eec4e5f8dbe7c124ee50c
Pablo Diego-Rosell <pablo_diego-rosell@gallup.com>
2020-05-11 15:33:05 +0200

    Update h17.R

--- cycle3/Boomtown/Analysis/h17.R
+++ cycle3/Boomtown/Analysis/h17.R
@@ -1,7 +1,7 @@
 ## Created by Pablo Diego Rosell, PhD, for Gallup inc. in September 2019
 
 # Formula
-formula.h17<-as.formula("inmot2~support+framing+complexity+timeUncertainty+pressure+tolerance+competition+centralization+leaderWeight+density+(1|player)+(1|group)")
+formula.h17<-as.formula("inmot2~support+framing+complexity+timeUncertainty+pressure+tolerance+competition+centralization+leaderWeight+density+(1|player)")
 
 # Extract number of prior parameters ('ndim') to be declared
 fittedGlmer <- stan_glmer(formula.h17, data=factorial, family=binomial(link="logit"), iter=3, chains=1)

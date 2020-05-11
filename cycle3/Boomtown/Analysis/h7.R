0e17cde9d3f61f3c376cc6a0ef88e976d8482fcb
Pablo Diego-Rosell <pablo_diego-rosell@gallup.com>
2020-05-11 14:41:34 +0200

    Update h7.R

--- cycle3/Boomtown/Analysis/h7.R
+++ cycle3/Boomtown/Analysis/h7.R
@@ -1,7 +1,7 @@
 ## Created by Pablo Diego Rosell, PhD, for Gallup inc. in September 2019
 
 # Formula
-formula.h7<-as.formula("inmot1~framing*compStrong+timeUncertainty+tools+tolerance+support+centralization+leaderWeight+pressure+density+(1|player)+(1|group)")
+formula.h7<-as.formula("inmot1~framing*compStrong+timeUncertainty+tools+tolerance+support+centralization+leaderWeight+pressure+density+(1|player)")
 
 # Extract number of prior parameters ('ndim') to be declared
 

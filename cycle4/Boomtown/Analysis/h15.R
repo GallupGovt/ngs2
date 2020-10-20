bf75ebf3db518482bfb169f845bdaccb905528d3
Pablo Diego-Rosell <pablo_diego-rosell@gallup.com>
2020-10-20 15:04:40 +0200

    Update h15.R

--- cycle4/Boomtown/Analysis/h15.R
+++ cycle4/Boomtown/Analysis/h15.R
@@ -12,7 +12,7 @@ formula.h15.1<-as.formula("conformity~tolerance+competition+pressure+grmot1+fram
 
 # Extract number of prior parameters ('ndim') to be declared
 
-fittedGlmer <- suppressWarnings(stan_glmer(formula.h15.1, data=factorialGroup, family=gaussian(link = "identity"), iter=3, chains=1))
+fittedGlmer <- suppressWarnings(stan_glmer(formula.h15.1, data=factorialGroup, family=gaussian(link = "identity"), iter=3, chains=1, refresh=0))
 ndim<-length(fittedGlmer$covmat[1,])-2
 
 # Declare priors

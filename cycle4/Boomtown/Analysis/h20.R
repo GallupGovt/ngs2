c9a9225dba7f967cafa2669ba1071da6fd24baab
Pablo Diego-Rosell <pablo_diego-rosell@gallup.com>
2020-10-20 11:38:26 +0200

    Update h20.R

--- cycle4/Boomtown/Analysis/h20.R
+++ cycle4/Boomtown/Analysis/h20.R
@@ -1,5 +1,12 @@
 ## Created by Pablo Diego Rosell, PhD, for Gallup inc. in September 2019
 
+# Environment
+source("functions.R")
+source("prep.R")
+
+# Iterations
+nIter <- 50000
+
 # Formula
 formula.h20<-as.formula("inmot2~centralization+density+conformity+grmot1+support+framing+complexity+timeUncertainty+pressure+tolerance+competition+leaderWeight+(1|player)")
 
@@ -31,3 +38,6 @@ BFs <- data.frame(20, test_1_0)
 colnames(BFs) <- c("Hypothesis",
                     "Prediction 1 vs. Null")
 write.csv(BFs, paste(od, "BFs20.csv", sep = '/'))                      
+
+summarize_delete ("bayesGlmer_h20_h20.0")
+summarize_delete ("bayesGlmer_h20_h20.1")

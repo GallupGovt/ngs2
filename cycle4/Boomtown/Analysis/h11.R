a2cfce28f9b3ecab96b65fdebea3862822479b35
Pablo Diego-Rosell <pablo_diego-rosell@gallup.com>
2020-10-20 15:03:51 +0200

    Update h11.R

--- cycle4/Boomtown/Analysis/h11.R
+++ cycle4/Boomtown/Analysis/h11.R
@@ -12,7 +12,7 @@ formula.h11<-as.formula("inmot1~framing*complexity+timeUncertainty+pressure+tole
 
 # Extract number of prior parameters ('ndim') to be declared
 
-fittedGlmer <- suppressWarnings(stan_glmer(formula.h11, data=factorial, family=binomial(link="logit"), iter=3, chains=1))
+fittedGlmer <- suppressWarnings(stan_glmer(formula.h11, data=factorial, family=binomial(link="logit"), iter=3, chains=1, refresh=0))
 ndim<-length(fittedGlmer$covmat[1,])-3
 
 # Declare priors

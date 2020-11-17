7099b91e8875df3b1bf5b88e1b4f3901570919e7
Pablo Diego-Rosell <pablo_diego-rosell@gallup.com>
2020-11-17 19:49:53 +0100

    Update h3.R

--- cycle4/Boomtown/Analysis/h3.R
+++ cycle4/Boomtown/Analysis/h3.R
@@ -5,7 +5,7 @@ source("functions.R")
 source("prep.R")
 
 # Iterations
-nIter <- 50000
+nIter <- 100000
 
 # Formula
 formula.h3<-as.formula("inmot1~risk+prb+competition+tolerance+support+structure+pressure+framing+density+timeUncertainty+(1|player)") #Formula h1.null

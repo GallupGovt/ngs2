66f2c2b0e04ea2e39f9a1cd7b2b55b3b7feef972
Pablo Diego-Rosell <pablo_diego-rosell@gallup.com>
2020-11-17 19:51:04 +0100

    Update h13.R

--- cycle4/Boomtown/Analysis/h13.R
+++ cycle4/Boomtown/Analysis/h13.R
@@ -5,7 +5,7 @@ source("functions.R")
 source("prep.R")
 
 # Iterations
-nIter <- 30000
+nIter <- 100000
 
 # Formula
 formula.h13.1<-as.formula("conformity~pressure+grmot1+framing+complexity+timeUncertainty+tolerance+competition+support+centralization+leaderWeight+density+(1|group)")
